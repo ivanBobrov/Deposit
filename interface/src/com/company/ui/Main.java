e402e6c18de0bd7ff6058d33309888e733d7fc1c
ivan <imponderabilidad@yandex.ru>
2016-08-03 18:32:18 +0300

    small fix

--- interface/src/com/company/ui/Main.java
+++ interface/src/com/company/ui/Main.java
@@ -4,15 +4,16 @@ import java.awt.*;
 import javax.swing.*;
 
 public class Main {
-    private static final Dimension frameSize = new Dimension(500, 600);
+    private static final Dimension FRAME_SIZE = new Dimension(500, 600);
+    private static final String WINDOW_NAME = "Deposit";
 
     public static void main(String[] args) {
-        JFrame mainFrame = new JFrame("Deposit");
+        JFrame mainFrame = new JFrame(WINDOW_NAME);
         JPanel choosePanel = new ChooseAccountPanel();
         mainFrame.getContentPane().add(choosePanel);
 
         mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        mainFrame.setMinimumSize(frameSize);
+        mainFrame.setMinimumSize(FRAME_SIZE);
         mainFrame.setVisible(true);
     }
 }
